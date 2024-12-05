dc3974f99fd62f84ea3b47783541d995aad7f41e
Pavel Budka <p.budka@gmail.com>
2024-12-05 21:18:20 +0100

    day4 1st solution

--- day4.py
+++ day4.py
@@ -1,6 +1,8 @@
 import re
 
 pattern = re.compile('XMAS')
+pattern2 = re.compile('MAS')
+
 
 def findXmas(lines):
 #    print(lines)
@@ -9,6 +11,16 @@ def findXmas(lines):
         total += len(re.findall(pattern, l))
     print(total)
     return total
+
+
+def findMas(lines):
+    #    print(lines)
+    total = 0
+    for l in lines:
+        total += len(re.findall(pattern2, l))
+    print(total)
+    return total
+
 
 def reverse(lines):
     rlines = []
@@ -61,12 +73,45 @@ def findAllXmas(lines):
     total += findXmas(reverse(dlines))
     return total
 
+def conversionTable(size):
+    list = [[(y,x) for x in range(size)] for y in range(size)]
+    return list
+
+tdiagonal1 = []
+tdiagonal2 = []
+
+def findAllMas(lines):
+    global tdiagonal1, tdiagonal2
+    conv = conversionTable(len(lines))
+    tdiagonal1 = diagonal(conv)
+    tdiagonal1 += reversed(diagonal(reverse(transpose(reverse(conv))))[:-1])
+    print(tdiagonal1)
+    tdiagonal2 = diagonal(reverse(conv))
+    tdiagonal2 += reversed(diagonal(reverse(transpose(conv)))[:-1])
+    print(tdiagonal1)
+    total = 0
+    dlines = diagonal(lines)
+    dlines += reversed(diagonal(reverse(transpose(reverse(lines))))[:-1])
+    print("diagonal1",  dlines)
+    total += findXmas(dlines)
+    total += findXmas(reverse(dlines))
+    dlines = diagonal(reverse(lines))
+    dlines += reversed(diagonal(reverse(transpose(lines)))[:-1])
+    print("diagonal2",  dlines)
+    total += findXmas(dlines)
+    total += findXmas(reverse(dlines))
+    return total
+
 def readLines(fname):
     with open(fname, "r") as f:
         lines = [l[:-1] for l in f.readlines()]
     print(len(lines), len(lines[0]))
     print(lines)
     return lines
+
+
+
+
 
 if __name__ == '__main__':
     inp = ["MMMSXXMASM",
@@ -79,7 +124,9 @@ if __name__ == '__main__':
            "SAXAMASAAA",
            "MAMMMXMMMM",
            "MXMXAXMASX"]
-    print(findAllXmas(inp))
-    print(findAllXmas(readLines("day4.txt")))
+#    print(findAllXmas(inp))
+    print(findAllMas(inp))
+#    print(findAllXmas(readLines("day4.txt")))
+print(conversionTable(4))
 
 
