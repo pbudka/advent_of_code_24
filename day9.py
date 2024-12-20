2895d9dbab1b92cc70c577332f1c052d642d0a98
Pavel Budka <p.budka@gmail.com>
2024-12-20 21:57:49 +0100

    day9 done

--- day9.py
+++ day9.py
@@ -33,10 +33,8 @@ def moveWholeString(data):
     i = 0
     for word in list(re.finditer(digit, string))[::-1]:
         i += 1
-        if i % 1 == 0:
+        if i % 1000 == 0:
             print(string[0:200], i, word.start(), word.group(0))
-        if i > 20:
-            return  string
         wordLen = word.end() - word.start()
         space = re.search(r'[.]{' + str(wordLen) + r',}', string[0:word.start()])
         if space:
@@ -47,37 +45,89 @@ def moveWholeString(data):
                       string[word.start() + wordLen:])
     print(string)
     return string
+
+class NumIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data) - 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self.index:
+            raise StopIteration
+        val = None
+        end = None
+        data = self.data
+        for i in range(self.index, -1, -1):
+            if not data[i] is None:
+                if val is None:
+                    val = data[i]
+                    end = i
+                elif val != data[i]:
+                    self.index = i
+                    return val, i+1, end
+            elif not val is None:
+                self.index = i
+                return val, i+1, end
+        else:
+            if not val is None:
+                self.index = 0
+                return val, 0, end
+
+
+class NoNeIterator:
+    def __init__(self, data, size):
+        self.data = data
+        self.size = size
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        data = self.data
+        while True:
+            try:
+                self.index = index = data.index(None, self.index)
+            except ValueError:
+                raise StopIteration
+            if index + self.size > len(data):
+                self.index = len(data)
+                raise StopIteration
+            for i in range(index, index + self.size):
+                if data[i]:
+                    self.index = i
+                    break
+            else:
+                self.index = index + self.size
+                break
+        return index
+
 
 def moveWhole(data):
-    ### replace regex with a search in a list
-    string = ''.join(['.' if i is None else str(i) for i in data])
-    print(string)
-    digit = re.compile(r'(0+|1+|2+|3+|4+|5+|6+|7+|8+|9+)')
     i = 0
-    for word in list(re.finditer(digit, string))[::-1]:
+    for num, s, e in NumIterator(data[:]):
+        ln = e - s + 1
         i += 1
-        if i % 1 == 0:
-            print(string[0:200], i, word.start(), word.group(0))
-        if i > 20:
-            return  string
-        wordLen = word.end() - word.start()
-        space = re.search(r'[.]{' + str(wordLen) + r',}', string[0:word.start()])
-        if space:
-            string = (string[:space.start()] +
-                      word.group(0) +
-                      string[space.start() + wordLen:word.start()] +
-                      '.' * wordLen +
-                      string[word.start() + wordLen:])
-    print(string)
-    return string
-
-
+        if i % 1000 == 0:
+            print(num, s, e, data[:100], data[-100:])
+        for space in NoNeIterator(data, ln):
+            if space < s:
+                for i in range(space, space + ln):
+                    data[i] = num
+                for i in range(s, e + 1):
+                    data[i] = None
+            break
+    return data
 
 
 def checksum(data):
     s = 0
-    for i in range(0, data.index(None)):
-        s += i * data[i]
+    for i in range(0, len(data)):
+        if data[i]:
+            s += i * data[i]
     return s
 
 def checksum2(data):
@@ -110,13 +160,18 @@ if __name__ == '__main__':
     print(data)
     data = moveWhole(data)
     print(data)
-    print(checksum2([int(i) if i != '.' else 0 for i in data]))
+    print(checksum(data))
 
     data = uncompress("233313312141413140225")
     print(data)
+    data = moveWhole(data)
+    print(data)
+    print(checksum(data))
 
     data = read("day9.txt")
     data = uncompress(data)
-#    data = moveWhole(data)
-#    print(checksum2([int(i) if i != '.' else 0 for i in data]))
+    print(data[:100], data[-100:])
+    data = moveWhole(data)
+    print(data[:100], data[-100:])
+    print(checksum(data))
     print(90546720886)
