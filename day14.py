72647dac64acec63a3dbaaa4cfddb259b558a524
Pavel Budka <p.budka@gmail.com>
2024-12-29 19:59:50 +0100

    day 14.1 done

--- day14.py
+++ day14.py
@@ -38,10 +38,48 @@ def read(fname):
     return robots
 
 
-def move(robots, n, x, y):
+def printr(robots, x, y, outFile):
+    grid = [['.'] * x for _ in range(y)]
+    for r in robots:
+        grid[r.y][r.x] = "o"
+    for l in grid:
+        outFile.write("".join(l) + "\n")
+
+
+def move(robots, n, x, y, outFile=None):
     for i in range(n):
+        rpos = set()
         for r in robots:
             r.move(x, y)
+            rpos.add((r.x, r.y))
+        if outFile is None:
+            continue
+        rpos = list(rpos)
+        maxx = 0
+        row = 0
+        prev = (-10, -10)
+        for el in sorted(rpos, key=lambda e: e[0] * 1000 + e[1]):
+            if el[0] == prev[0] and el[1] == prev[1] + 1:
+                row += 1
+            else:
+                if row > maxx:
+                    maxx = row
+                row = 0
+            prev = el
+        maxy = 0
+        prev = (-10, -10)
+        row = 0
+        for el in sorted(rpos, key=lambda e: e[0] + e[1] * 1000):
+            if el[1] == prev[1] and el[0] == prev[0] + 1:
+                row += 1
+            else:
+                if row > maxy:
+                    maxy = row
+                row = 0
+            prev = el
+        if maxx > 10 or maxy > 10:
+            outFile.write(str(i) + "\n")
+            printr(robots, x, y, outFile)
 
 
 def count(robots, x, y):
@@ -65,6 +103,7 @@ if __name__ == '__main__':
 
     robots = read("day14.txt")
     print(robots)
-    move(robots, 100, 101, 103)
+    with open("day14out.txt", "wt") as f:
+        move(robots, 100000, 101, 103, f)
     print(robots)
     print(count(robots, 101, 103))
